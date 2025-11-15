a6965ab0312c0fb4e98ac08521798d20ce175df0
Guilherme Samuel <guisamuel800@gmail.com>
2025-11-15 12:31:36 -0300

    feat: create player base movement

---
+++ scripts/player.gd
@@ -0,0 +1,64 @@
+class_name Player extends CharacterBody2D
+
+var move_speed : float = 100.0
+var cardinal_direction : Vector2 = Vector2.DOWN
+var direction : Vector2 = Vector2.ZERO
+enum PlayerState {
+	IDLE,
+	RUN,
+	ATTACK
+}
+var state: PlayerState = PlayerState.IDLE
+@onready var sprite: AnimatedSprite2D = $AnimatedSprite2D
+
+func _ready() -> void:
+	sprite.play("idle_down")
+
+func _process(delta: float) -> void:
+	direction.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+	direction.y = Input.get_action_raw_strength("ui_down") - Input.get_action_strength("ui_up")
+	
+	velocity = direction * move_speed
+	
+	if update_direction() || update_state():
+		update_animation()
+
+func _physics_process(delta: float) -> void:
+	move_and_slide()
+
+func update_state() -> bool:
+	var new_state : PlayerState = PlayerState.IDLE if direction == Vector2.ZERO else PlayerState.RUN
+	if new_state == state:
+		return false
+	state = new_state
+	return true
+
+func update_animation() -> void:
+	var animation_state : String = "idle"
+	if state == PlayerState.RUN:
+		animation_state = "run"
+	sprite.play(animation_state + "_" + animation_direction())
+
+func update_direction() -> bool:
+	var new_direction : Vector2 = cardinal_direction
+	if direction == Vector2.ZERO:
+		return false
+	
+	if direction.y == 0:
+		new_direction = Vector2.LEFT if direction.x < 0 else Vector2.RIGHT
+	elif direction.x == 0:
+		new_direction = Vector2.UP if direction.y < 0 else Vector2.DOWN
+	
+	if new_direction == cardinal_direction:
+		return false
+	cardinal_direction = new_direction
+	sprite.scale.x = -1 if cardinal_direction == Vector2.LEFT else 1
+	return true
+
+func animation_direction() -> String:
+	if cardinal_direction == Vector2.DOWN:
+		return "down"
+	elif cardinal_direction == Vector2.UP:
+		return "up"
+	else:
+		return "side"
