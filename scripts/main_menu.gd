5566a8a45cd4473aa3940d49fccff4069bf82680
fernandofelxs <fernando.felix@unifesp.br>
2025-12-01 17:21:04 -0300

    refatorar layout da UI

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -4,4 +4,4 @@ func _on_play_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/levels/room407.tscn")
 
 func _on_quit_pressed() -> void:
-	get_tree().quit() # Somente funciona em um jogo exportado, e não no editor
+	get_tree().quit() 
