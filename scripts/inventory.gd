b9e82ed4f3febb2d30fb31105817578b3ff76f23
fernandofelxs <fernando.felix@unifesp.br>
2025-12-01 17:05:12 -0300

    adicionar componentes para o sistema de inventário

---
+++ scripts/inventory.gd
@@ -0,0 +1,20 @@
+extends Node
+
+var slots := [null, null]  # dois slots por personagem
+
+func add_item(item: String) -> bool:
+	for i in range(slots.size()):
+		if slots[i] == null:
+			slots[i] = item
+			return true
+	return false  # inventário cheio
+
+func remove_item(item: String) -> bool:
+	for i in range(slots.size()):
+		if slots[i] == item:
+			slots[i] = null
+			return true
+	return false
+
+func has_item(item: String) -> bool:
+	return item in slots
