b5f53c68ac10d088a7f3c02fd4bc238e08c7ca24
fernandofelxs <fernando.felix@unifesp.br>
2025-12-18 15:35:37 -0300

    implementar pong

---
+++ levels/pong.gd
@@ -0,0 +1,15 @@
+extends Sprite2D
+
+const PADDLE_SPEED : int = 600
+
+func _on_ball_timer_timeout() -> void:
+	$Ball.new_ball()
+
+func _on_left_body_entered(body: Node2D) -> void:
+	if body == $Ball:
+		$BallTimer.start()
+
+
+func _on_right_body_entered(body: Node2D) -> void:
+	if body == $Ball:
+		$BallTimer.start()
